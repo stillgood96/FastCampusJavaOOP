119e03f26fb3a81589c87017f00706103f13ba44
stillgood96 <a787574@naver.com>
2022-11-19 20:20:26 +0900

    02. 생활 속에서 객체 찾아 클래스로 구현해 보기

---
+++ src/ch02/lecture02/Student.java
@@ -0,0 +1,10 @@
+package ch02.lecture02;
+
+public class Student {
+  // 객체의 속성은 클래스의 멤버 변수( member variable )로 선언함.
+  int studentNumber;
+  String studentName;
+  int majorCOde;
+  String majorName;
+  int grade;
+}
